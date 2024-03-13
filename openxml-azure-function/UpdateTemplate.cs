2a2d73b193a0acc75ce4d977b155eddd5a22411d
skraymo1 <kyleraymond@microsoft.com>
2024-03-13 06:09:16 -0500

    Added additional templates

--- openxml-azure-function/UpdateTemplate.cs
+++ openxml-azure-function/UpdateTemplate.cs
@@ -49,6 +49,27 @@ namespace OpenXMLFunction
                         case "Test System":
                             ReplaceSections(ref body, "##TESTSYSTEM##", section.Value);
                             break;
+                        case "TESTING FACILITY":
+                            ReplaceSections(ref body, "##TESTING FACILITY##", section.Value);
+                            break;
+                        case "Test Article":
+                            ReplaceSections(ref body, "##Test Article##", section.Value);
+                            break;
+                        case "Good Laboratory Practice Compliance":
+                            string glp = "";
+                            if (section.Value.ToLower().Contains("this study was not conducted in accordance") || section.Value.ToLower().Contains("this study will not be conducted in accordance"))
+                            {
+                                glp = "This study was not conducted in accordance with the Code of Federal Regulations (CFR), Title 21, Part 58: Good Laboratory Practice (GLP) for Nonclinical Laboratory Studies, issued by the United States Food and Drug Administration (FDA). This study was conducted as a basic exploratory study and as such, the data from this study were not audited by Quality Assurance (QA). However, all data were recorded appropriately and documentation necessary for reconstruction of this study is available in the study files at Takeda Development Center Americas, Inc. (TDCA) (San Diego, CA, USA). The data are accurately reflected in the report.";
+                            }
+                            else
+                            {
+                                glp = "This study was conducted in accordance with the Code of Federal Regulations, Title 21, Part 58: Good Laboratory Practice for Nonclinical Laboratory Studies, issued by the United States Food and Drug Administration. The study was conducted to meet GLP standards, and all data were audited by Quality Assurance to ensure accuracy and compliance. The necessary documentation for reconstruction of this study is available in the study files at Takeda Development Center Americas, Inc. (TDCA) (CITY, ST, USA). The data presented in this report accurately reflect the findings of the study.";
+                            }
+                            ReplaceSections(ref body, "##GOODLABCOMPLIANCE##", glp);
+                            break;
+                        case "ARCHIVING":
+                            ReplaceSections(ref body, "##ARCHIVING##", section.Value);
+                            break;
                         default:
                             //Non standard headers to be evaluated here.
                             if (section.Key.ToLower().StartsWith("document title"))
@@ -91,7 +112,7 @@ namespace OpenXMLFunction
             // Loop through each header part
             foreach (HeaderPart headerPart in headerParts)
             {
-        
+
                 foreach (var currentText in headerPart.RootElement.Descendants<Text>())
                 {
                     if (currentText.Text.Contains("TAK-"))
